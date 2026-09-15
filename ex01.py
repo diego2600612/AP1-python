9a3d9b61dd12438fc9d92d1bc93dbf36f6671ec5
diego2600612 <diego.2600612@aluno.impacta.edu.br>
2026-09-15 16:30:38 -0300

    Enhance ex01.py with explanatory comments
    Added comments to clarify variable usage and output.

--- ex01.py
+++ ex01.py
@@ -1,17 +1,18 @@
 def mostrar_resultados():
     nome = "Python"
     print(nome)
-
+    #falando quem a variavel nome = Python
     resultado = 2 + 3 * 4
     print(resultado)
-
+    #print(resultado) vai somar a conta e retornar apenas o resultado
     mensagem = "Olá, Diego!"
     print(mensagem)
-
+    #retornar a mensagem Olá, Diego
     texto = "2 + 3 * 4"
     print(texto)
-
+    #ele vai retornar o texto com a conta
     print(f"{texto} = {resultado}")
-
+    #retorna o texto no caso a conta dizendo que é igual ao resulto
 
 mostrar_resultados()
+
